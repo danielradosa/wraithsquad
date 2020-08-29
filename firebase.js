9378d4d922ab26137b232b7ab5c2b9f5d22520c8
Daniel Radosa <danysko5@gmail.com>
2020-08-29 15:27:48 +0200

    firebase SDK implementation

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB8xqlNTWcU6feo50WVT93v6NKNzfRABuA",
+    authDomain: "wraithsquad-test.firebaseapp.com",
+    databaseURL: "https://wraithsquad-test.firebaseio.com",
+    projectId: "wraithsquad-test",
+    storageBucket: "wraithsquad-test.appspot.com",
+    messagingSenderId: "154708062144",
+    appId: "1:154708062144:web:ac5e8f9f776ffdd8e72225"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
